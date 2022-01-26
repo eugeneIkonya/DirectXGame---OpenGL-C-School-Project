a4bc8b51f7f8ba663f125b823a459b74aa2b2f31
Michael Owen Oduor <michaeloduor50@gmail.com>
2022-01-27 00:21:19 +0300

    Add project files.

---
+++ Window.cpp
@@ -0,0 +1,19 @@
+#include "Window.h"
+
+Window::Window()
+{
+
+}
+bool Window::init()
+{
+	return false;
+}
+
+bool Window::release()
+{
+	return false;
+}
+
+Window::~Window()
+{
+}
---
+++ Window.h
@@ -0,0 +1,12 @@
+#pragma once
+class Window
+{
+public:
+	Window();
+	//Initialize the window
+	bool init();
+	//Release the window
+	bool release();
+	~Window();
+};
+
